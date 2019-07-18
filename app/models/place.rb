9647889e2847114d77dbfff8648dcf74d911094f
Alexandra Videll <alexandra.videll@gmail.com>
2019-07-18 23:37:11 +0000

    maps

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,5 @@ class Place < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode 
 	validates :name, :address, :description, presence: true
-	validates :name, length: {minimum: 3}
+	validates :name, length: {minimum: 3, maximum: 140}
 end
